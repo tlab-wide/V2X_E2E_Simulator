92024f050a3cd4fc8265face805cc54aec16b322
KASHANI <amkkashani@gmail.com>
2024-05-13 11:25:12 +0330

    fix: reduction in extra libraries

--- Assets/KashiwaPackage/Script/MockSensor.cs
+++ Assets/KashiwaPackage/Script/MockSensor.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using std_msgs.msg;
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UIElements;
